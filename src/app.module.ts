86c9981bfc6f27f5b3d3fccb6b3a0ef0cb55f467
Nilotpal Deka <nilotpaldeka@techvariable.com>
2023-01-19 10:18:33 +0530

    Add typeorm configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { EnvironmentValidationSchema } from './common/config-validation.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { __prod__ } from './common/common.constant';
+import { EnvironmentValidationSchema } from './common/schema/env-validation.schema';
 import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validationSchema: EnvironmentValidationSchema }),
+    TypeOrmModule.forRoot({
+      port: 5432,
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: '12345',
+      synchronize: !__prod__,
+      autoLoadEntities: true,
+      database: 'project-management',
+    }),
     IamModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
