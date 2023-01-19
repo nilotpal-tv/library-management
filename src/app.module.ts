d9832a0079735dca101399b5db02d89df814467f
Nilotpal Deka <nilotpaldeka@techvariable.com>
2023-01-19 13:30:45 +0530

    Removed typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { __prod__ } from './common/common.constant';
 import { IamModule } from './iam/iam.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    IamModule,
-    PrismaModule,
-  ],
+  imports: [ConfigModule.forRoot(), IamModule, PrismaModule],
 })
 export class AppModule {}
