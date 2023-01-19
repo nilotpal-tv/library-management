6019c38cee1f0c1a32ce15f77e36b2080cd3d787
Nilotpal Deka <nilotpaldeka@techvariable.com>
2023-01-19 16:59:07 +0530

    Remove reader and librarian query.

--- src/iam/authentication/guards/access-token.guard.ts
+++ src/iam/authentication/guards/access-token.guard.ts
@@ -1,23 +1,10 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { USER_TYPE_KEY } from '../decorators/user-type.decorator';
-import { UserType } from '../enums/user-type.enum';
 import { TokenService } from '../services/token.service';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  constructor(
-    private readonly reflector: Reflector,
-    private readonly tokenService: TokenService,
-    private readonly prismaService: PrismaService,
-  ) {}
+  constructor(private readonly tokenService: TokenService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
@@ -25,26 +12,8 @@ export class AccessTokenGuard implements CanActivate {
     if (!token) return false;
 
     const payload = await this.tokenService.verifyToken(token, 'ACCESS_TOKEN');
-    const userType = this.reflector.getAllAndOverride(USER_TYPE_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ])[0] as UserType;
-
-    if (userType === UserType.Librarian) {
-      const librarian = await this.prismaService.librarian.findUnique({
-        where: { id: payload.sub },
-      });
-
-      if (!librarian) throw new UnauthorizedException();
-      return true;
-    }
-
-    const reader = await this.prismaService.reader.findUnique({
-      where: { id: payload.sub },
-    });
-
-    if (!reader) throw new UnauthorizedException();
-    return true;
+    if (payload) return true;
+    return false;
   }
 
   private extractTokenFromHeder(request: Request): string | undefined {
