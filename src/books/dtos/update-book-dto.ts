3e1aee4d3ef4ba3fb7a3e709de683a281543fe55
Nilotpal Deka <nilotpaldeka@techvariable.com>
2023-01-20 18:03:35 +0530

    Make count optional

--- src/books/dtos/update-book-dto.ts
+++ src/books/dtos/update-book-dto.ts
@@ -1,9 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, IsPositive } from 'class-validator';
+import { IsNumber, IsPositive, IsOptional } from 'class-validator';
 import { AddBookDto } from './add-book.dto';
 
 export class UpdateBookDto extends PartialType(AddBookDto) {
   @IsNumber()
   @IsPositive()
+  @IsOptional()
   count?: number;
 }
